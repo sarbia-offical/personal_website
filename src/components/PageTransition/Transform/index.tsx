198a6dd23d3b4f1ebcdbb42355524f6e5d7b6ddb
WwwanQin <xm36447864@126.com>
2025-02-05 11:58:31 +0800

    fix: check page transform issues

--- src/components/PageTransition/Transform/index.tsx
+++ src/components/PageTransition/Transform/index.tsx
@@ -3,8 +3,6 @@ import type { ReactNode } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 
-import { variants } from './animation';
-
 interface IProps {
     children: ReactNode;
 }
@@ -14,11 +12,14 @@ export const Transform = ({ children }: IProps) => {
     return (
         <AnimatePresence mode="wait">
             <motion.div
-                key={router.asPath}
-                variants={variants}
-                initial="initial"
-                animate="animate"
-                exit="exit"
+                key={router.pathname}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+                onAnimationComplete={(definition) => {
+                    console.log('动画完成阶段:', definition);
+                }}
             >
                 {children}
             </motion.div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,23 +11,23 @@ export default function App({ Component, pageProps, router }: AppProps) {
     return (
         <div className={$styles.outer_layout}>
             <AppInfoStore>
-                <CapsuleNavigation
-                    hrefList={[
-                        {
-                            href: '/about',
-                            title: 'About',
-                        },
-                        {
-                            href: '/gallery',
-                            title: 'Gallery',
-                        },
-                        {
-                            href: '/archive',
-                            title: 'Archive',
-                        },
-                    ]}
-                />
                 <Transform>
+                    <CapsuleNavigation
+                        hrefList={[
+                            {
+                                href: '/about',
+                                title: 'About',
+                            },
+                            {
+                                href: '/gallery',
+                                title: 'Gallery',
+                            },
+                            {
+                                href: '/archive',
+                                title: 'Archive',
+                            },
+                        ]}
+                    />
                     <Component {...pageProps} />
                 </Transform>
             </AppInfoStore>
