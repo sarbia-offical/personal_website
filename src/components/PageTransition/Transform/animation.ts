58184899aec032d03762c1b30bff8251d9dc0d4f
WwwanQin <xm36447864@126.com>
2025-02-05 10:32:34 +0800

    fix: check page transform issues

--- src/components/PageTransition/Transform/animation.ts
+++ src/components/PageTransition/Transform/animation.ts
@@ -1,13 +1,4 @@
 export const variants = {
-    init: {
-        opacity: 0,
-    },
-    animate: {
-        opacity: 1,
-        transition: {
-            duration: 0.5,
-        },
-    },
     exit: {
         x: 20,
         opacity: 0,
