53c5f906753ea24a96d8d2ad1ed799bf307fd208
WwwanQin <xm36447864@126.com>
2025-02-05 12:21:02 +0800

    feat: remove page transform

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app';
 
 import CapsuleNavigation from '@/components/CapsuleNavigation';
-import { Transform } from '@/components/PageTransition/Transform';
 import '@/styles/index.scss';
 import { AppInfoStore } from '@/zustand';
 
@@ -11,25 +10,23 @@ export default function App({ Component, pageProps, router }: AppProps) {
     return (
         <div className={$styles.outer_layout}>
             <AppInfoStore>
-                <Transform>
-                    <CapsuleNavigation
-                        hrefList={[
-                            {
-                                href: '/about',
-                                title: 'About',
-                            },
-                            {
-                                href: '/gallery',
-                                title: 'Gallery',
-                            },
-                            {
-                                href: '/archive',
-                                title: 'Archive',
-                            },
-                        ]}
-                    />
-                    <Component {...pageProps} />
-                </Transform>
+                <CapsuleNavigation
+                    hrefList={[
+                        {
+                            href: '/about',
+                            title: 'About',
+                        },
+                        {
+                            href: '/gallery',
+                            title: 'Gallery',
+                        },
+                        {
+                            href: '/archive',
+                            title: 'Archive',
+                        },
+                    ]}
+                />
+                <Component {...pageProps} />
             </AppInfoStore>
         </div>
     );
